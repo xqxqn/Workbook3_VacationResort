0921bf1f8c6590dbcefb13e8fbc8013f5ffb42ef
Phillip Nhan <phillipnhan2023@gmail.com>
2023-10-29 21:16:36 -0400

    added reference links on homepage and created index.css

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//window.onload = function(){
+//    let flavorSubmitBtnEl = document.getElementById("flavor-submit-btn");
+//    flavorSubmitBtnEl.onclick = function(){
+//        let flavorNameEl = document.getElementById("flavor-name");
+//        console.log(flavorNameEl.value);
+//    }
+//}
+
+
+window.onload = function(){
+    let flavorForm.onsubmit = document.getElementById("flavor-form");
+    flavorForm.onsubmit = function(e){
+        e.preventDefault();
+        console.log("Trigger");
+    }
+
+
+
+}
